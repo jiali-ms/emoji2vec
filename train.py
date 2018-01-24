ef53601f49fafb62bfa221fdf90a069f148a67cb
Jiali Yao <jiayao@microsoft.com>
2018-01-24 15:28:37 +0900

    add initial code and data

---
+++ train.py
@@ -0,0 +1,22 @@
+from gensim.models import *
+import itertools
+import pickle
+
+def train_model(debug=False):
+    if debug:
+        train = [['hello', 'word'],
+                 ['how', 'are', 'you']]
+    else:
+        with open('data/corpus.txt', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            train = [line.strip().split() for line in lines]
+
+    # normally set embedding size between 10-200
+    # window size is range of context you would like to include
+    # a wider range windows size will let the vector learn more about topic
+    model = Word2Vec(train, size=10, window=5, min_count=0, workers=4)
+    model.save('data/word2vec.bin')
+    model.wv.save_word2vec_format('data/word2vec.txt', binary=False)
+
+if __name__ == "__main__":
+    train_model(debug=False)
